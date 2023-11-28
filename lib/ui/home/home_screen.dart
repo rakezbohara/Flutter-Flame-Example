24c1baf563bef4c43d7ec1fa7ebd7b83aa9eaada
Rakesh Bohara <rakesh@8squarei.com>
2023-11-28 15:16:13 +0545

    refactor: remove boilerplate code

---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'HomeScreen is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
